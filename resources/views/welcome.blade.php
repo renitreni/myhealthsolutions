ceaf259bb9981c5d5bdcc8b71fa8436ff39b38e1
Renier Trenuela <renier.trenuela@Renier-Trenuelas-Mac.local>
2025-01-30 15:05:10 +0800

    Update footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -570,12 +570,11 @@
             <div class="container">
                 <div class="row align-items-center">
                     <p class="footer-text m-0 col-lg-8 col-md-12">
-                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                        Copyright &copy;
+                        &copy;
                         <script>
                             document.write(new Date().getFullYear());
-                        </script> All rights reserved
-                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        </script> AMDC. All Rights Reserved. Developed By Smart Inbox Technology
+                        <!-- Website Author: Renier R. Trenuela -->
                     </p>
                     <div class="col-lg-4 col-md-12 text-center text-lg-right footer-social">
                         <a href="#"><i class="ti-facebook"></i></a>
